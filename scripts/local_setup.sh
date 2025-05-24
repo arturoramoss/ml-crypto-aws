508e3fa0435f389872eccf32fd44a3aee52575d1
Arturo <arti.ramos.s@gmail.com>
2025-05-24 12:29:07 +1000

    Initial commit

---
+++ scripts/local_setup.sh
@@ -0,0 +1,5 @@
+# first run assuming you have conda
+# Require python 3.12
+pip config set global.trusted-host 'pypi.org files.pythonhosted.org pypi.python.org'
+pipenv install --dev
+pipenv run pre-commit install
